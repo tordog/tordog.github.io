d6e7732427d54d2106d7396cc0e36b0d0eec27f6
tordog <Nielsen@Victorias-MBP.wireless.yale.internal>
2015-10-11 01:33:49 -0400

    Change to personal website 2015

--- script.js
+++ script.js
@@ -5,5 +5,14 @@ $(document).ready(function() {
 	$("li").click(function(){
 		$("#moremenu").toggle(300);
 	});
+
+	$(window).resize(function() {
+		if($(window).width() < 735){
+			$(".title").addClass("whiteText");
+		}
+		else{
+			$(".title").removeClass("whiteText");
+		}
+	})
 });
 
